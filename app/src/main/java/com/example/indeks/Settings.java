ade6d90ad529b8d48885104b2767546dde7b8047
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2019-11-11 10:47:41 +0100

    Little corrections line.

--- app/src/main/java/com/example/indeks/Settings.java
+++ app/src/main/java/com/example/indeks/Settings.java
@@ -60,7 +60,6 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
