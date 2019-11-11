83b6d3a18546ae23de978ce1dffbb6f486a8d252
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2019-11-11 10:49:03 +0100

    Add subject of study with registration user to database.

--- app/src/main/java/com/example/indeks/RegistrationInfo.java
+++ app/src/main/java/com/example/indeks/RegistrationInfo.java
@@ -28,6 +28,8 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class RegistrationInfo extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener {
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private String userID;
 
     private DatabaseReference databaseReference;
 
@@ -42,6 +44,8 @@ public class RegistrationInfo extends AppCompatActivity implements View.OnClickL
     private Spinner spinner;
     private Button buttonSave;
 
+    public static String study;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +56,9 @@ public class RegistrationInfo extends AppCompatActivity implements View.OnClickL
             finish();
             startActivity(new Intent(RegistrationInfo.this, MainActivity.class));
         }
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userID = user.getUid();
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Students");
 
@@ -75,8 +82,7 @@ public class RegistrationInfo extends AppCompatActivity implements View.OnClickL
     }
 
     private void saveUserInformation() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String userID = user.getUid();
+
         String email = user.getEmail();
 
         String name = editTextName.getText().toString().trim();
@@ -88,13 +94,13 @@ public class RegistrationInfo extends AppCompatActivity implements View.OnClickL
         String birthdayDate = editTextBirthdayDate.getText().toString().trim();
         String phoneNumber = editTextPhoneNumber.getText().toString().trim();
 
-        String study = String.valueOf(spinner.getSelectedItem());
+        study = String.valueOf(spinner.getSelectedItem());
 
         UserInformation userInformation = new UserInformation(name, surname, pesel, street, city, postalAddress, birthdayDate, study, phoneNumber, email, userID);
 
         databaseReference.child(userID).child("User_Information").setValue(userInformation);
-        databaseReference.child(userID).child("Image_Information").child("Name").setValue("Empty").toString();
-        databaseReference.child(userID).child("Image_Information").child("URL").setValue("Empty").toString();
+        databaseReference.child(userID).child("Image_Information").child("Name").setValue("Empty");
+        databaseReference.child(userID).child("Image_Information").child("URL").setValue("Empty");
 
         Toast.makeText(this, "Zapisywanie danych...", Toast.LENGTH_SHORT).show();
 
@@ -102,10 +108,83 @@ public class RegistrationInfo extends AppCompatActivity implements View.OnClickL
         startActivity(new Intent(RegistrationInfo.this, Home.class));
     }
 
+    public static String getStudy() {
+        return study;
+    }
+
+    private void saveGrades() {
+        if (study.equals("Informatyka Stosowana")) {
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("Students").child(userID).child("Grades").child("InformatykaStosowana");
+
+            databaseReference.child("Semestr_1").child("Matematyka I").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Lektorat języka angielskiego I").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Ochrona własności intelektualnej").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Podstawy użytkowania systemów komputerowych").setValue("Empty");
+            databaseReference.child("Semestr_1").child("WF").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Wstęp do informatyki").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Wstęp do programowania").setValue("Empty");
+            databaseReference.child("Semestr_1").child("Wstęp do pomiarów i automatyki").setValue("Empty");
+            databaseReference.child("Semestr_1").child("_SEMESTR_1_ZALICZONY_?").setValue("TAK");
+
+            databaseReference.child("Semestr_2").child("Matematyka II").setValue("Empty");
+            databaseReference.child("Semestr_2").child("Lektorat języka angielskiego II").setValue("Empty");
+            databaseReference.child("Semestr_2").child("Fizyka").setValue("Empty");
+            databaseReference.child("Semestr_2").child("Algorytmy i programowanie").setValue("Empty");
+            databaseReference.child("Semestr_2").child("WF").setValue("Empty");
+            databaseReference.child("Semestr_2").child("Podstawy elektrotechniki i elektroniki").setValue("Empty");
+            databaseReference.child("Semestr_2").child("Architektura komputerów").setValue("Empty");
+            databaseReference.child("Semestr_2").child("_SEMESTR_2_ZALICZONY_?").setValue("NIE");
+
+            databaseReference.child("Semestr_3").child("Algorytmy i struktury danych").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Bazy danych").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Lektorat języka angielskiego III").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Matematyka dyskretna").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Podstawy techniki mikroprocesorowej").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Prawo informatyczne").setValue("Empty");
+            databaseReference.child("Semestr_3").child("Sieci komputerowe").setValue("Empty");
+            databaseReference.child("Semestr_3").child("_SEMESTR_3_ZALICZONY_?").setValue("NIE");
+
+            databaseReference.child("Semestr_4").child("Etyka biznesu i etyki zawodowe").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Lektorat języka angielskiego IV").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Podstawy automatyki i robotyki").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Podstawy inżynierii oprogramwoania").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Podstawy metod probabilistycznych i statystyki").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Podstawy sztucznej inteligencji").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Systemy wbudowane").setValue("Empty");
+            databaseReference.child("Semestr_4").child("Użytkowanie oprogramowania inżynierskiego").setValue("Empty");
+            databaseReference.child("Semestr_4").child("_SEMESTR_4_ZALICZONY_?").setValue("NIE");
+
+            databaseReference.child("Semestr_5").child("Elementy grafiki komputerowej i przetwarzania obrazu").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Elementy administracji baz danych").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Kurs C").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Kurs Java").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Systemy operacyjne i programowania systemowe").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Srodowiska i narzędzia wytwarzania oprogramowania").setValue("Empty");
+            databaseReference.child("Semestr_5").child("Wstęp do przedsiębiorczości").setValue("Empty");
+            databaseReference.child("Semestr_5").child("_SEMESTR_5_ZALICZONY_?").setValue("NIE");
+
+            databaseReference.child("Semestr_6").child("Kurs C++").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Kurs JavaScript").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Kurs programowania gier komputerowych z wykorzystaniem silnika Unity").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Pracownia inżynierska - część 1").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Pracownia programowania zespołowego - część 1").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Praktyka inżynierska").setValue("Empty");
+            databaseReference.child("Semestr_6").child("Proseminarium inżynierskie").setValue("Empty");
+            databaseReference.child("Semestr_6").child("_SEMESTR_6_ZALICZONY_?").setValue("NIE");
+
+            databaseReference.child("Semestr_7").child("Praca dyplomowa").setValue("Empty");
+            databaseReference.child("Semestr_7").child("Pracownia inżynierska - część 2").setValue("Empty");
+            databaseReference.child("Semestr_7").child("Pracownia programowania zespołowego - część 2").setValue("Empty");
+            databaseReference.child("Semestr_7").child("Seminarium inżynierskie").setValue("Empty");
+            databaseReference.child("Semestr_7").child("_SEMESTR_7_ZALICZONY_?").setValue("NIE");
+        }
+    }
+
     @Override
     public void onClick(View v) {
         if (v == buttonSave) {
             saveUserInformation();
+            saveGrades();
         }
     }
 
