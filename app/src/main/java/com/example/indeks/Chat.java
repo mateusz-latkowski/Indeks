c92140dd1157b5109161468848cf863f63f9d374
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2019-11-13 21:09:11 +0100

    Create Chat Activity.

---
+++ app/src/main/java/com/example/indeks/Chat.java
@@ -0,0 +1,33 @@
+package com.example.indeks;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("CHAT");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean OnCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
