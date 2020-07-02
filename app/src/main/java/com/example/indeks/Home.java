a03d116af150d18b97ab784c83d9b15d9652c4b2
Mateusz <mateuszlatkowski@smcebi.edu.pl>
2020-07-02 12:07:14 +0200

    Update.

--- app/src/main/java/com/example/indeks/Home.java
+++ app/src/main/java/com/example/indeks/Home.java
@@ -8,10 +8,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
